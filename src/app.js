6a5858cba92477775bbdab3a31e1609d3b91c15d
Luciano Junior <junioristylo@gmail.com>
2026-01-27 20:44:13 -0300

    Adciona CORS

--- src/app.js
+++ src/app.js
@@ -3,6 +3,23 @@ const routes = require("./routes/index.js")
 
 
 const app = express()
+
+// 👇 ADICIONE O CORS AQUI (ANTES DAS ROTAS)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+}
+
+    next();
+});
+
+app.use(express.json()) //
+
 routes(app)
 
 
