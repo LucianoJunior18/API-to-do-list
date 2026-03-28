2a441ccd41be735963aa78fc57ab2ae7b5678f90
Luciano Junior <junioristylo@gmail.com>
2026-03-28 11:25:07 -0300

    fix: migrar para PostgreSQL

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,22 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: process.env.DB_DIALECT || 'sqlite',
-    storage: process.env.DB_STORAGE || './src/database/database.sqlite'
+    dialect: 'sqlite',
+    storage: './src/database/database.sqlite'
   },
   test: {
     dialect: 'sqlite',
     storage: './src/database/database.sqlite'
   },
   production: {
-    dialect: 'sqlite',
-    storage: './src/database/database.sqlite'
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
