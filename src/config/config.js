4a004ddc550656feb478b2735d58b0bb2043392d
Luciano Junior <junioristylo@gmail.com>
2026-03-28 11:42:34 -0300

    fix: parsear DATABASE_URL manualmente

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,26 @@
 require('dotenv').config();
 
+const { URL } = require('url');
+
+let productionConfig = {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: false
+  }
+};
+
+if (process.env.DATABASE_URL) {
+  const dbUrl = new URL(process.env.DATABASE_URL);
+  productionConfig = {
+    ...productionConfig,
+    username: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.slice(1),
+    host: dbUrl.hostname,
+    port: dbUrl.port,
+  };
+}
+
 module.exports = {
   development: {
     dialect: 'sqlite',
@@ -9,14 +30,5 @@ module.exports = {
     dialect: 'sqlite',
     storage: './src/database/database.sqlite'
   },
-  production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
+  production: productionConfig
 };
