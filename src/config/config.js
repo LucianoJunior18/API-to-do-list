e6b197c1fb7e83ab6e4064bfb27c680b9dce526c
Luciano Junior <junioristylo@gmail.com>
2026-03-28 12:06:24 -0300

    fix: usar use_env_variable no Sequelize

--- src/config/config.js
+++ src/config/config.js
@@ -1,26 +1,5 @@
 require('dotenv').config();
 
-const { URL } = require('url');
-
-let productionConfig = {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: false
-  }
-};
-
-if (process.env.DATABASE_URL) {
-  const dbUrl = new URL(process.env.DATABASE_URL);
-  productionConfig = {
-    ...productionConfig,
-    username: dbUrl.username,
-    password: dbUrl.password,
-    database: dbUrl.pathname.slice(1),
-    host: dbUrl.hostname,
-    port: dbUrl.port,
-  };
-}
-
 module.exports = {
   development: {
     dialect: 'sqlite',
@@ -30,5 +9,11 @@ module.exports = {
     dialect: 'sqlite',
     storage: './src/database/database.sqlite'
   },
-  production: productionConfig
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false
+    }
+  }
 };
